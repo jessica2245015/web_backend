4c4fa9cbdf18b77631b19949c4e0e1b89d5fd540
jessica2245015 <165674335+jessica2245015@users.noreply.github.com>
2024-04-15 17:01:22 +0700

    feature get profile current user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,44 +1,42 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Requests\UserLoginRequest;
-
-use App\http\Resources\UserResource;
+use App\Http\Resources\UserResource;
 use App\Models\User;
-
 use Illuminate\Http\Request;
-use Illuminate\http\JsonResponse;
-
+use Illuminate\Http\JsonResponse;
 use Illuminate\support\Str;
 use Illuminate\support\Facades\Hash;
-
+use Illuminate\support\Facades\Auth;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserController extends Controller
 {
-    public function register (UserRegisterRequest $request): JsonResponse
+    public function register(UserRegisterRequest $request): JsonResponse
     {
-        $user = User::Create($request->validated());
-
+        $user = User::create($request->validated());
         return (new UserResource($user))->response()->setStatusCode(201);
     }
-
     public function login(UserLoginRequest $request): UserResource
     {
         $data = $request->validated();
         $user = User::where('email', $data['email'])->first();
         if (!$user || !Hash::check($data['password'], $user->password)) {
-            throw new HttpresponseException (response([
+            throw new HttpResponseException (response([
                 'errors' => [
                     'message' => ['username or password wrong'],
                 ]
             ],401));
         }
-        
-        $user->remember_token = str::uuid()->toString();
+        $user->remember_token = Str::uuid()->toString();
         $user->save();
+        return new UserResource($user);
+    }
+
+    public function get(Request $request): UserResource
+    {
+        $user = Auth::user();
         return new UserResource($user);
     }
 }
