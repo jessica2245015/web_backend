183fd601d079415aa0e23a247f427170bdadb1ee
jessica2245015 <165674335+jessica2245015@users.noreply.github.com>
2024-04-22 14:02:20 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,17 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 // });
 Route::group(['prefix'=>'v1'],function(){
-    // api/v1/users
-    route::post('/users',[App\Http\Controllers\UserController::class,'register']);
-    route::post('/users/login',[App\Http\Controllers\UserController::class,'login']);
+    Route::post('/users',[App\Http\Controllers\UserController::class,'register']);
+    Route::post('/users/login',[App\Http\Controllers\UserController::class,'login']);
+
+    Route::middleware(App\Http\Middleware\ApiAuthMiddleware::class)->group(function (){
+        Route::get('/users/profile',[App\Http\Controllers\UserController::class,'get']);
+        Route::patch('/users/profile',[App\Http\Controllers\UserController::class,'update']);
+        Route::delete('/users/logout',[App\Http\Controllers\UserController::class,'logout']);
+    });
+
+    Route::post('/contacts', [App\Http\Controllers\ContactsController::class, 'create']);
+    Route::patch('/contacts/{id}', [App\Http\Controllers\ContactsController::class, 'update']);
+    Route::delete('/contacts/{id}', [App\Http\Controllers\ContactsController::class, 'delete']);
+    Route::get('/contacts/search', [App\Http\Controllers\ContactsController::class, 'search']);
 });
